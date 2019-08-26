e48b56fb916b1aaa94b008b9f26674116f81e7c7
discordjszone <54225918+discordjszone@users.noreply.github.com>
2019-08-25 21:49:46 -0500

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,17 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+ 
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setActivity('', { type: 'PLAYING' })
+});
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+client.login(process.env.BOT_TOKEN);
